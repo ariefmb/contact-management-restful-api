026819287cd8eebbe54ea37b51baceaa10ef1e8a
Arief Budiman <mariefbudiman12@gmail.com>
2026-01-12 21:17:54 +0700

    add cors for cross platform

--- src/application/web.js
+++ src/application/web.js
@@ -1,11 +1,13 @@
+import cors from "cors"
 import express from 'express'
 import { errorMiddleware } from '../middleware/error-middleware.js'
-import { publicRouter } from '../route/public-api.js'
 import { userRouter } from '../route/api.js'
+import { publicRouter } from '../route/public-api.js'
 
 export const web = express()
 
 web.use(express.json())
+web.use(cors())
 
 web.use(publicRouter)
 web.use(userRouter)
